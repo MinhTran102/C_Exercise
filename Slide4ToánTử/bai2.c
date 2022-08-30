cc85626f7a7a652cbadef6f34081e06746878d36
Tran Duc Minh <minhtd@lumi.vn>
2022-08-30 11:56:41 +0700

    o

--- Slide4ToánTử/bai2.c
+++ Slide4ToánTử/bai2.c
@@ -1,1 +1,11 @@
 //Đặt bit thứ 5 thành giá trị 1
+#include<stdio.h>
+int main()
+{
+    unsigned int a;
+    unsigned int b;
+    printf("Nhap so doi bit thu 5: \n");
+    scanf("%o", &a);
+    
+    return 0;
+}
