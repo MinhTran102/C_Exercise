7a94632227789fedb34218a537cc4cc05ff4364f
Tran Duc Minh <minhtd@lumi.vn>
2022-08-30 11:05:54 +0700

    xong bài 1 slide 4

--- Slide4ToánTử/bai1.c
+++ Slide4ToánTử/bai1.c
@@ -1,1 +1,16 @@
 //Kiểm tra bit thứ 3 là 1 hay 0
+#include<stdio.h>
+int main()
+{
+    unsigned int a;
+    printf("Nhap so kiem tra: \n");
+    scanf("%o", &a);
+    a = a >> 2;
+    if (a%2==1) {
+        printf("Bit thu 3 la 1");
+    } 
+        else {
+            printf("Bit thu 3 la 0");
+        }
+    return 0;
+}
